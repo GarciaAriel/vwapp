c2237332178a5e39d51f4ff2c7dd7f950f986bf7
mauriciofp <mauriciofp@outlook.com>
2015-04-20 16:50:33 -0400

    modif a scheduler

--- www/js/constants/constants_contact.js
+++ www/js/constants/constants_contact.js
@@ -4,3 +4,10 @@ angular.module('starter.constantscontact',[])
 .constant('apiUrlLocal', 'http://localhost:8080/bm')
 
 .constant('pathContact', '/bmapp/Contact/REST.do')
+
+//CONSTANTS FOR CONTACTS
+//type of contact
+// Contact person>1
+.constant('PersonType',1)
+// contact Organiztion or company> 0
+.constant('OrgType',0);
