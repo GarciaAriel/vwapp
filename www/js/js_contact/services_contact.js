bece110f504cf0c36fd8ba19491acf3bcc33a7b3
armandoaliaga <armandoaliaga92@gmail.com>
2015-06-11 17:45:23 -0400

    first version of uploading image from camera and gallery

--- www/js/js_contact/services_contact.js
+++ www/js/js_contact/services_contact.js
@@ -55,6 +55,7 @@ angular.module('starter.contactservices', [])
   function transformRequest( data, getHeaders ) {
     var headers = getHeaders();
     headers[ "Content-type" ] = "application/x-www-form-urlencoded; charset=utf-8";
+    headers[ "enctype" ] = "multipart/form-data";
     return( serializeData( data ) );
   }
   // Return the factory value.
