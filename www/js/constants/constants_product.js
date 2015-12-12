c3dbc5144298bbddf5770f9b1e00f9b1cc17d215
garciaariel <ags123123@gmail.com>
2015-12-11 22:51:23 -0400

    first

--- www/js/constants/constants_product.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.constantsproduct',[])  
-
-// get product detail
-.constant('DETAIL_PRODUCT_URL','/bmapp/Product/Forward/Update.do')
-
-// get list products
-.constant('LIST_PRODUCTS_URL','/bmapp/Product/List.do');
